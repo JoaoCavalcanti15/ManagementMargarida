ac07b4068a9dd39bbbf4ed6f5d4314995fe73f99
jaojao <uni.joaoassis@gmail.com>
2024-07-13 05:21:22 +0100

    changed package.json for Netlify usage

--- public/src/index.js
+++ public/src/index.js
@@ -9,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/InflatableManagement">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
